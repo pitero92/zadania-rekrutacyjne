2cf8508b69f26baa9566c4699c72f0423197364f
pitero <piorrt@gmail.com>
2019-08-10 14:39:05 +0200

    changed lower() position

--- zad10.py
+++ zad10.py
@@ -2,10 +2,10 @@
 def anagram(first_word, second_word):
 
 
-    first_word_sorted = (''.join(sorted(first_word))).lower()
-    second_word_sorted = (''.join(sorted(second_word))).lower()
+    first_word_sorted = ''.join(sorted(first_word.lower()))
+    second_word_sorted = ''.join(sorted(second_word.lower()))
 
     return first_word_sorted == second_word_sorted
 
 
-print(anagram("finder", "friend"))
+print(anagram("finder", "Friend"))
