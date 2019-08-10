37adc4d4f8ac37884526b780e0d6a05d89bfefea
pitero <piorrt@gmail.com>
2019-08-10 12:31:01 +0200

    cake dividing

---
+++ cake.py
@@ -0,0 +1,9 @@
+
+def equal_slices(total_slices, no_recipients, slices_each):
+    if (total_slices >= no_recipients*slices_each):
+        print("Zjemy")
+
+    else:
+        print("Nie zjemy")
+
+equal_slices(11,5,2)
