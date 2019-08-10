cdcc1476f197dd5ff551b4ab0903625a36a53f53
pitero <piorrt@gmail.com>
2019-08-10 14:40:40 +0200

    delete join function

--- zad10.py
+++ zad10.py
@@ -2,8 +2,8 @@
 def anagram(first_word, second_word):
 
 
-    first_word_sorted = ''.join(sorted(first_word.lower()))
-    second_word_sorted = ''.join(sorted(second_word.lower()))
+    first_word_sorted = sorted(first_word.lower())
+    second_word_sorted = sorted(second_word.lower())
 
     return first_word_sorted == second_word_sorted
 
